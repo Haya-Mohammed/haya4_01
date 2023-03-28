d4fa2d74c719bca55db307014bd5e5107b7f2989
Haya-Mohammed <haya37162@gmail.com>
2023-03-28 14:25:16 +0300

    commit 2

--- BMI_calculator/lib/modules/bmi_result/bmi_result_screen.dart
+++ BMI_calculator/lib/modules/bmi_result/bmi_result_screen.dart
@@ -9,6 +9,14 @@ class ResultPage extends StatelessWidget {
 
   ResultPage({required this.isMale,required this.height,required this.weight,required this.age,required this.result});
 
+  String health(result){
+    if(result<18.5) return "Underweight";
+    else if(18.5<=result && result<24.9) return "Normal weight";
+    else if(25.0<=result && result<29.9) return "NOverweight";
+        else {
+      return 'Obesity';
+    }
+  }
   @override
   Widget build(BuildContext context) {
     String gender = isMale ? 'Male' : 'Female' ;
@@ -29,7 +37,9 @@ class ResultPage extends StatelessWidget {
             Text('Weight :  $weight',style: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.white)),
             Text('Age :  $age',style: const TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.white)),
             const SizedBox(height: 100),
-            Text('Your BMI is $result',style: const TextStyle(fontSize: 35,fontWeight: FontWeight.bold,color: Colors.white)),
+            Text('Your BMI is : $result',style: const TextStyle(fontSize: 35,fontWeight: FontWeight.bold,color: Colors.white)),
+            Text('"${health(result)}"',style: const TextStyle(fontSize: 35,fontWeight: FontWeight.bold,color: Colors.red)),
+
           ],
         ),
       ),
